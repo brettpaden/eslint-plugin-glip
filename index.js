6b8898b5144baf1cdbd3414dae565d8608947c08
Ian Cole <ian.cole.dev@gmail.com>
2018-02-14 15:54:11 -0500

    Add more rules and lint package

--- index.js
+++ index.js
@@ -1,87 +1,49 @@
-'use strict'
+'use strict';
 
 module.exports = {
-	rules: {
-		'no-camelcase': (context) => ({
-			VariableDeclarator: (node) => {
-				if (node.id.name) {
-					const first_character = node.id.name.charAt(0);
-					const chars = node.id.name.split('');
-					if (first_character === first_character.toLowerCase()) {
-						for (let i = 1, n = chars.length; i < n; i++) {
-							const char = chars[i];
-							if (char === '_') {
-								continue;
-							}
-							if (char === char.toUpperCase()) {
-								context.report(node, 'Variables may not contain camelCase');
-							}
-						}
-					}
-				}
-			}
-		}),
-		'no-func-param-named-arguments': (context) => {
-			const check_parameters = (node) => {
-				if (node && node.params && node.params.length) {
-					node.params.forEach((param_node) => {
-						if (param_node && param_node.name && param_node.name === 'arguments') {
-							context.report(
-								param_node,
-								'Function parameter named \'arguements\' is not allowed'
-							);
-						}
-					});
-				}
-			};
-			const rule = {
-				FunctionDeclaration: check_parameters,
-				FunctionExpression: check_parameters,
-				ArrowFunctionExpression: check_parameters,
-			};
-			return rule;
-		}
-	},
+	rules: require('./rules.js'),
 	configs: {
 		base: {
-			'rules': {
+			rules: {
 				'glip/no-camelcase': 'error',
 				'glip/no-func-param-named-arguments': 'error',
 				'spaced-comment': 'off',
 				'no-tabs': 'off',
-				'indent': ['error', 'tab'],
-				'camelcase': 'off',
+				indent: ['error', 'tab'],
+				camelcase: 'off',
 				'new-cap': 'off', // i.e. Errors.Invalid_Type() should only be used if Errors.Invalid_Type() is a constructor
 				'prefer-destructuring': 'off',
 				'arrow-parens': 'off', // arrow parrens around single parameter arrow functions
 				'consistent-return': 'off',
 				'brace-style': ['error', 'stroustrup'], // Disallows  "} else {"
 				'comma-dangle': ['error', {
-					'arrays': 'only-multiline',
-					'objects': 'only-multiline',
-					'imports': 'always',
-					'exports': 'always',
-					'functions': 'never'
+					arrays: 'only-multiline',
+					objects: 'only-multiline',
+					imports: 'always',
+					exports: 'always',
+					functions: 'never'
 				}],
 				'max-len': ['error', {
-					'code': 100,
-					'ignoreTrailingComments': true,
-					'ignoreStrings': true,
-					'ignoreTemplateLiterals': true,
-					'ignoreRegExpLiterals': true
+					code: 100,
+					ignoreTrailingComments: true,
+					ignoreStrings: true,
+					ignoreTemplateLiterals: true,
+					ignoreRegExpLiterals: true
 				}],
 				'import/no-dynamic-require': 'off',
 				'space-before-function-paren': ['error', 'always'],
-				'strict': 'off',
+				strict: 'off',
 				'import/newline-after-import': 'off',
-				'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+				'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
 				'func-style': ['error', 'expression'],
-				'no-loop-func': 'error',
+				'no-loop-func': 'error', // Disallows function declarations inside of loops
+				'multiline-ternary': ['error', 'always-multiline'],
+				'no-continue': 'off', // Allows use of continue statements
+				'global-require': 'off',
 			},
 		},
 		server_es6: require('./server_es6.js'),
 		server_es5: require('./server_es5.js'),
 		web_es5: require('./web_es5.js')
 	}
-}
-
+};
--- server_es6.js
+++ server_es6.js
@@ -1,20 +1,20 @@
 module.exports = {
-    'rules': {
-        'no-var': 'warn',
-        'prefer-template': 'warn',
-        'prefer-arrow-callback': 'warn',
-        'object-shorthand': ['error', 'methods'],
-    },
-    'env': {
-        'node': true,
-    },
-    'parserOptions': {
-        'ecmaVersion': 6,
-    },
-    'plugins': ['glip'],
-    'extends': [
-        'airbnb-base',
-        'plugin:glip/base',
-    ],
-    'globals': require('./server_globals.js')
-}
+	rules: {
+		'no-var': 'warn',
+		'prefer-template': 'warn',
+		'prefer-arrow-callback': 'warn',
+		'object-shorthand': ['error', 'methods'],
+	},
+	env: {
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 6,
+	},
+	plugins: ['glip'],
+	extends: [
+		'airbnb-base',
+		'plugin:glip/base',
+	],
+	globals: require('./server_globals.js')
+};
